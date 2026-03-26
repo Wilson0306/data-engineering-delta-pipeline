9d0aae195b9e3fc51967bd33a5e221cde9cd801a
Wilson0306 <wilson.tony@diggibyte.com>
2026-03-26 10:00:59 +0530

    Update 01.raw_to_bronze.py

--- notebooks/01.raw_to_bronze.py
+++ notebooks/01.raw_to_bronze.py
@@ -1,5 +1,9 @@
 # Databricks notebook source
 # DBTITLE 1,Cell 1
+%run ./00.config
+
+# COMMAND ----------
+
 dbutils.widgets.text("load_type", "")
 load_type = dbutils.widgets.get("load_type")
 
@@ -9,16 +13,16 @@ from pyspark.sql.functions import current_timestamp, max
 
 # COMMAND ----------
 
-if load_type == "incremental" and spark.catalog.tableExists("wilson.bronze.sales"):
-    
-    last_time = spark.table("wilson.silver.sales") \
-                     .select(max("ingestion_time")) \
+if load_type == "incremental" and spark.catalog.tableExists(BRONZE_TABLE):
+
+    last_time = spark.table(BRONZE_TABLE) \
+                     .select(max(WATERMARK_COL)) \
                      .collect()[0][0]
-    df = spark.table("wilson.raw.sales")                 
+    df = spark.table(RAW_TABLE)
     df = df.filter(df.ingestion_time > last_time)
-    df_bronze = df.withColumn("ingestion_time", current_timestamp())
-    df_bronze.write.format("delta").mode("append").option("overwriteSchema", "true").saveAsTable("wilson.bronze.sales")
+    df_bronze = df.withColumn(WATERMARK_COL, current_timestamp())
+    df_bronze.write.format("delta").mode("append").saveAsTable(BRONZE_TABLE)
 else:
-     df = spark.table("wilson.raw.sales")
-     df_bronze = df.withColumn("ingestion_time", current_timestamp())
-     df_bronze.write.format("delta").mode("overwrite").option("overwriteSchema", "true").partitionBy("order_id").saveAsTable("wilson.bronze.sales")
+    df = spark.table(RAW_TABLE)
+    df_bronze = df.withColumn(WATERMARK_COL, current_timestamp())
+    df_bronze.write.format("delta").mode("overwrite").partitionBy(BRONZE_PARTITION_COL).saveAsTable(BRONZE_TABLE)
