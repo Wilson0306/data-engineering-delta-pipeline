df75237cc134fa7991ef8404d9040eb0f5fce1e0
Wilson0306 <wilson.tony@diggibyte.com>
2026-03-26 10:01:55 +0530

    Update 03.silver_to_gold.py

--- notebooks/03.silver_to_gold.py
+++ notebooks/03.silver_to_gold.py
@@ -1,49 +1,49 @@
 # Databricks notebook source
+%run ./00.config
+
+# COMMAND ----------
+
 dbutils.widgets.text("load_type", "")
 load_type = dbutils.widgets.get("load_type")
 
 # COMMAND ----------
 
 from delta.tables import DeltaTable
-from pyspark.sql.functions import col
-from pyspark.sql.functions import monotonically_increasing_id
-
+from pyspark.sql.functions import col, sha2, concat_ws
 
 # COMMAND ----------
 
 # DBTITLE 1,Dim
-df = spark.table("wilson.silver.sales")
+df = spark.table(SILVER_TABLE)
 
 dim = df.select("product", "category").dropDuplicates()
 
-dim = dim.withColumn("product_id", monotonically_increasing_id())
+dim = dim.withColumn("product_id", sha2(concat_ws("|", col("product"), col("category")), 256))
 
-dim.write.format("delta").mode("overwrite").saveAsTable("wilson.gold.dim_product")
+dim.write.format("delta").mode("overwrite").saveAsTable(GOLD_DIM)
 
 # COMMAND ----------
 
 # DBTITLE 1,Fact
-df = spark.table("wilson.silver.sales")
-dim = spark.table("wilson.gold.dim_product")
+df = spark.table(SILVER_TABLE)
+dim = spark.table(GOLD_DIM)
 
 fact = df.join(dim, ["product", "category"], "left")
 
 fact = fact.withColumn("revenue", col("price") * col("quantity"))
 
-target_table = "wilson.gold.fact_sales"
-
-if load_type == "historical":
-    fact.write.format("delta").mode("overwrite").saveAsTable(target_table)
-
-else:
-    if not spark.catalog.tableExists(target_table):
-        fact.write.format("delta").saveAsTable(target_table)
+if load_type == "incremental":
+    if not spark.catalog.tableExists(GOLD_FACT):
+        fact.write.format("delta").saveAsTable(GOLD_FACT)
     else:
-        delta_table = DeltaTable.forName(spark, target_table)
+        delta_table = DeltaTable.forName(spark, GOLD_FACT)
 
         delta_table.alias("target").merge(
             fact.alias("source"),
-            "target.order_id = source.order_id"
+            FACT_MERGE_KEY
         ).whenMatchedUpdateAll() \
          .whenNotMatchedInsertAll() \
          .execute()
+
+else:
+    fact.write.format("delta").mode("overwrite").saveAsTable(GOLD_FACT)
